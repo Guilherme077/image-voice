0844f9a6d8c37beda803c4b87c4f1de8ec1385c9
Guilherme731 <gui.pilz.2007@gmail.com>
2025-05-25 10:53:12 -0300

    Insert message bits in image and save it

--- imagevoice/encode.py
+++ imagevoice/encode.py
@@ -14,5 +14,23 @@ def encodeMessage(message, imageUrl):
     imageArrayRGB = numpy.array(inputImage) # Obtém uma matriz tridimensional dos valores RGB da imagem (linhas, colunas, RGB)
     imagePixels = imageArrayRGB.flatten() # Tranforma a matriz tridimensional em uma lista única de valores RGB
 
-    print("A imagem possui capacidade de ", len(imagePixels), " bits")
+    print("A imagem possui capacidade de ", len(imagePixels), " bits de mensagem")
     print("A mensagem possui tamanho de ", len(messageBinary), " bits")
+
+    # Inserir bits da mensagem na imagem
+    for bitIndex in len(messageBinary):
+        originalPixelImage = imagePixels[bitIndex] # Valor original do canal de cor do pixel (RGB)
+        bitMessage = messageBinary[bitIndex] # Bit que será registrado na imagem
+        
+        # Se o bit que será registrado é 0 e o valor do canal de cor da imagem é ímpar...
+        if(bitMessage == 0 and originalPixelImage % 2 != 0):
+            originalPixelImage += 1 # O valor do canal de cor deve ser par
+
+        # Se o bit que será registrado é 1 e o valor do canal de cor da imagem é par...
+        if(bitMessage == 1 and originalPixelImage % 2 == 0):
+            originalPixelImage += 1 # O valor do canal de cor deve ser ímpar
+
+    # Converter os valores para arquivo de imagem e salvar
+    imageEncoded = imagePixels.reshape(imageArrayRGB.shape)
+    imageWithMessage = Image.fromarray(imageEncoded.astype('uint8'))
+    imageWithMessage.save("encoded_img.png")
