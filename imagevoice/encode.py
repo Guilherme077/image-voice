2a91552f041d8b74052c5beba0f3fae0b17228b9
Guilherme731 <gui.pilz.2007@gmail.com>
2025-09-05 16:23:21 -0300

    Ensure the uint8 type use while inserting message data

--- imagevoice/encode.py
+++ imagevoice/encode.py
@@ -11,8 +11,9 @@ def encodeMessage(message, imageUrl, imageOutputUrl):
 
     # Processamento da imagem
     inputImage = Image.open(imageUrl) # Abre a imagem a partir do nome
-    imageArrayRGB = numpy.array(inputImage) # Obtém uma matriz tridimensional dos valores RGB da imagem (linhas, colunas, RGB)
+    imageArrayRGB = numpy.array(inputImage, dtype=numpy.uint8) # Obtém uma matriz tridimensional dos valores RGB da imagem (linhas, colunas, RGB)
     imagePixels = imageArrayRGB.flatten() # Tranforma a matriz tridimensional em uma lista única de valores RGB
+    imagePixels = imagePixels.astype(numpy.uint8)
 
     print("A imagem possui capacidade de ", len(imagePixels), " bits de mensagem")
     print("A mensagem possui tamanho de ", len(messageBinary), " bits")
@@ -20,7 +21,7 @@ def encodeMessage(message, imageUrl, imageOutputUrl):
     # Inserir bits da mensagem na imagem
     for bitIndex in range(len(messageBinary)):
         bit = int(messageBinary[bitIndex]) # Bit que será registrado na imagem
-        imagePixels[bitIndex] = (imagePixels[bitIndex] & ~1) | bit
+        imagePixels[bitIndex] = ((imagePixels[bitIndex] & (~1 & 0xFF)) | bit).astype(numpy.uint8)
 
     # Converter os valores para arquivo de imagem e salvar
     imageEncoded = imagePixels.reshape(imageArrayRGB.shape)
