7a099ec5f34c9d958ccc78be725ed169f6991c17
Guilherme077 <gui.pilz.2007@gmail.com>
2025-10-28 21:27:12 -0300

    Add encode/decode time count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from imagevoice.encode import encodeMessage
 from imagevoice.decode import decodeFromImage
 from imagevoice.tools import textToSpeech
+import time
 
 print("-----IMAGE VOICE-----")
 print("1 - Codificar Mensagem em Imagem")
@@ -12,11 +13,17 @@ if option == "1":
     imageUrl = input("Digite o nome da imagem (com tipo de arquivo): ")
     message = input("Digite uma mensagem: ")
     imageOutputUrl = input("Digite o nome da imagem (com tipo de arquivo): ")
+    startedAt = time.time()
     encodeMessage(message, imageUrl, imageOutputUrl)
+    timeToExecute = time.time() - startedAt
+    print("Essa operação levou " + str(round(timeToExecute, 3)) + " segundos para ser concluída.")
 elif option == "2":
     imageUrl = input("Digite o nome da imagem (com tipo de arquivo): ")
+    startedAt = time.time()
     decodedText = decodeFromImage(imageUrl)
+    timeToExecute = time.time() - startedAt
     print(decodedText)
+    print("Essa operação levou " + str(round(timeToExecute, 3)) + " segundos para ser concluída.")
     textToSpeech(decodedText)
 
 
